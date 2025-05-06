9752210ef1fdc51863d01c211a1f801d3a86c118
JoaoAlves20 <jvsimoesalves@gmail.com>
2025-05-06 14:59:45 -0300

    updated server.js

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { router } from "./routes/router.js";
 const server = express();
 
 server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 server.use(helmet());
 
 server.use('/users', router);
